83703cf2a4d54a5c5fe867ed68fcc175ab18b90a
User <mobiplus25@gmail.com>
2025-03-03 10:03:35 +0300

    list has been edited

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ const routes = [
     {
         path: '/',
         name: 'Home',
-        component: () => import('@/components/DataList.vue')
+        component: () => import('@/views/Index.vue')
     },
     {
         path: '/user-matrix/:userId',
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -5,7 +5,7 @@ import DataList from '../components/DataList.vue'
 <template>
   <div class="min-h-screen bg-gray-100">
     <div class="container mx-auto py-8">
-      <h1 class="text-3xl font-bold text-center mb-8">Firebase Veri Listesi</h1>
+      <h1 class="text-3xl font-bold text-center mb-8">Firebase Data List</h1>
       <DataList />
     </div>
   </div>
